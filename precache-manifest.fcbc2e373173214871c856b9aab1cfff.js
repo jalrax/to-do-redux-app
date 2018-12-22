640890c3d1393fd9041e10704c2b487d533e9660
jalrax <trymental@gmail.com>
2018-12-23 00:05:43 +0300

    Updates

---
+++ precache-manifest.fcbc2e373173214871c856b9aab1cfff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7f815804b89f3a5eb7",
+    "url": "/to-do-redux-app/static/js/runtime~main.8e7f8158.js"
+  },
+  {
+    "revision": "0f73fa7a674d2494e034",
+    "url": "/to-do-redux-app/static/js/main.0f73fa7a.chunk.js"
+  },
+  {
+    "revision": "24df0e8006dcf2a2282c",
+    "url": "/to-do-redux-app/static/js/1.24df0e80.chunk.js"
+  },
+  {
+    "revision": "0f73fa7a674d2494e034",
+    "url": "/to-do-redux-app/static/css/main.4dcfeec1.chunk.css"
+  },
+  {
+    "revision": "0e62397413fda5d9ab339cdf37d97787",
+    "url": "/to-do-redux-app/index.html"
+  }
+];
